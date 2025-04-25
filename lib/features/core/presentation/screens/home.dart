535d2bb0393bbec59a523c57f9bf699124a39875
Biniyam Kinfe <biniyamkinfe21@gmail.com>
2025-04-25 16:43:51 -0700

    adding bottom nav bar and bloc

---
+++ lib/features/core/presentation/screens/home.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:safe_campus/features/core/presentation/bloc/NavigationCubit.dart';
+import 'package:safe_campus/features/core/presentation/screens/HomePage.dart';
+import 'package:safe_campus/features/core/presentation/screens/alertPage.dart';
+import 'package:safe_campus/features/core/presentation/screens/mapPage.dart';
+import 'package:safe_campus/features/core/presentation/screens/profilePage.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final int index = 0;
+
+  final List pages = [HomePage(), Mappage(), Alertpage(), Profilepage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NavigationCubit, int>(
+      builder: (context, selectedIndex) {
+        return Scaffold(
+          appBar: AppBar(title: Text("SafeCampus")),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            shape: CircleBorder(),
+            child: Text("SOS"),
+          ),
+          body: pages[selectedIndex],
+          bottomNavigationBar: BottomAppBar(
+            notchMargin: 5,
+            color: Color(0xFFE8DEF8),
+            height: 65,
+            shape: CircularNotchedRectangle(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                navItem(
+                  context,
+                  icon: Icons.home,
+                  label: "Home",
+                  index: 0,
+                  selected: selectedIndex,
+                ),
+                navItem(
+                  context,
+                  icon: Icons.search,
+                  label: "Search",
+                  index: 1,
+                  selected: selectedIndex,
+                ),
+                SizedBox(width: 40),
+                navItem(
+                  context,
+                  icon: Icons.message,
+                  label: "Messages",
+                  index: 2,
+                  selected: selectedIndex,
+                ),
+                navItem(
+                  context,
+                  icon: Icons.person,
+                  label: "Profile",
+                  index: 3,
+                  selected: selectedIndex,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget navItem(
+  BuildContext context, {
+  required IconData icon,
+  required String label,
+  required int index,
+  required int selected,
+}) {
+  final isSelected = selected == index;
+  final color = isSelected ? Colors.deepPurpleAccent : Colors.grey;
+
+  return GestureDetector(
+    onTap: () => context.read<NavigationCubit>().updateIndex(index),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 20, color: color),
+        SizedBox(height: 2),
+        Text(label, style: TextStyle(fontSize: 12, color: color)),
+      ],
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:safe_campus/features/core/presentation/bloc/NavigationCubit.dart';
+import 'package:safe_campus/features/core/presentation/screens/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BlocProvider(
+    create: (context) => NavigationCubit(),
+    child: Container(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +15,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
   }
 }
