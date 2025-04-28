5f55e90b6044350582e03142a041b9b398443f85
Trilord52 <tinbiteyonas52@gmail.com>
2025-04-28 10:43:35 +0300

    First page iterations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/core/presentation/providers/auth/auth_bloc.dart';
+import 'features/core/presentation/screens/intro_page.dart';
+import 'features/core/presentation/screens/sign_in_page.dart';
+import 'features/core/presentation/screens/register_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +14,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthBloc()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SafeCampus',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: 'Poppins',
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const IntroPage(),
+          '/signin': (context) => const SignInPage(),
+          '/register': (context) => const RegisterPage(),
+        },
+      ),
+    );
   }
 }
