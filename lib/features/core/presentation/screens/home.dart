f24434650a196ea7a837e117b19ac489b13da6f4
Trilord52 <tinbiteyonas52@gmail.com>
2025-05-02 01:08:41 +0300

    final fixed integrated code, before starting to integrate map

--- lib/features/core/presentation/screens/home.dart
+++ lib/features/core/presentation/screens/home.dart
@@ -15,62 +15,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final int index = 0;
-
-  final List pages = [HomePage(), Mappage(), Alertpage(), Profilepage()];
+  final List pages = [
+    HomePage(),
+    Mappage(),
+    Alertpage(),
+    Profilepage(),
+  ];
 
   void openDialogeBox() {
     showDialog(
       context: context,
-      builder:
-          (context) => AlertDialog(
-            backgroundColor: Colors.white,
-            title: Text(
-              "Confirm your request",
-              style: GoogleFonts.poppins(
-                fontWeight: FontWeight.w500,
-                fontSize: 22,
-              ),
-            ),
-            //content
-            content: SizedBox(
-              height: 280,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //image
-                  Image(image: AssetImage('assets/images/alert1.png')),
-                  //text
-                  Text(
-                    "The alert will be send to security persons and trusted contacts with your location and personal information. Make sure you made the right request before sending alert!",
-                    style: GoogleFonts.poppins(),
-                  ),
-                ],
-              ),
-            ),
-            //action
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("Cancle"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.red),
-                  shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ))
-                ),
-                child: Text(
-                  "Send Alert",
-                  style: GoogleFonts.poppins(color: Colors.white),
-                ),
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Confirm your request",
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
+            fontSize: 22,
+          ),
+        ),
+        content: SizedBox(
+          height: 280,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset('assets/images/alert1.png'),
+              Text(
+                "The alert will be sent to security personnel and trusted contacts with your location and personal information. Make sure you made the right request before sending alert!",
+                style: GoogleFonts.poppins(),
               ),
             ],
           ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("Cancel"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Add alert sending logic here
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text(
+              "Send Alert",
+              style: GoogleFonts.poppins(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -79,9 +80,8 @@ class _HomeState extends State<Home> {
     return BlocBuilder<NavigationCubit, int>(
       builder: (context, selectedIndex) {
         return Scaffold(
-          //appBar: AppBar(title: Text("SafeCampus"), automaticallyImplyLeading: false,),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
+          body: pages[selectedIndex], // <- THIS LINE ADDED
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
           floatingActionButton: FloatingActionButton(
             onPressed: openDialogeBox,
             backgroundColor: Colors.deepPurpleAccent,
@@ -91,9 +91,9 @@ class _HomeState extends State<Home> {
           ),
           bottomNavigationBar: BottomAppBar(
             notchMargin: 5,
-            color: Color.fromARGB(255, 223, 222, 236),
+            color: const Color.fromARGB(255, 223, 222, 236),
             height: 70,
-            shape: CircularNotchedRectangle(),
+            shape: const CircularNotchedRectangle(),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -111,7 +111,7 @@ class _HomeState extends State<Home> {
                   index: 1,
                   selected: selectedIndex,
                 ),
-                SizedBox(width: 40),
+                const SizedBox(width: 40),
                 navItem(
                   context,
                   icon: Icons.notifications,
@@ -143,21 +143,20 @@ Widget navItem(
   required int selected,
 }) {
   final isSelected = selected == index;
-  final color = isSelected ? Color(0xFF65558F) : const Color.fromARGB(255, 124, 124, 124);
+  final color = isSelected ? const Color(0xFF65558F) : const Color.fromARGB(255, 124, 124, 124);
 
   return InkWell(
     onTap: () => context.read<NavigationCubit>().updateIndex(index),
-
     child: Container(
       width: 60,
       height: 80,
-      decoration: BoxDecoration(color: Colors.transparent),
+      decoration: const BoxDecoration(color: Colors.transparent),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(icon, size: 25, color: color),
-          SizedBox(height: 2),
+          const SizedBox(height: 2),
           Text(label, style: TextStyle(fontSize: 12, color: color)),
         ],
       ),
